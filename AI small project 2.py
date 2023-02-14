a914f4515ee9a74796cafb338d2838928339fb39
Sifatnaseem <121250143+Sifatnaseem@users.noreply.github.com>
2023-02-14 11:22:06 -0500

    Add files via upload

---
+++ AI small project 2.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[46]:
+
+
+
+from sklearn import datasets
+
+#Load dataset
+cancer = datasets.load_breast_cancer()
+# print the names of the 13 features
+print("Features: ", cancer.feature_names)
+
+# print the label type of cancer('malignant' 'benign')
+print("Labels: ", cancer.target_names)
+
+
+# In[47]:
+
+
+# Import train_test_split function
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
+
+# Split dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, test_size=0.3,random_state=109) # 70% training and 30% test
+knn=KNeighborsClassifier()
+k_range = list(range(1,25))
+param_grid = dict(n_neighbors=k_range)
+grid_search=grid.fit(X_train,y_train)
+grid = GridSearchCV (knn, param_grid,cv=15,scoring='accuracy',return_train_score=False,verbose=1)
+print(grid_search.best_params_)
+
+
+# In[45]:
+
+
+knn=  KNeighborsClassifier(n_neighbors=8)
+knn.fit(X_train, y_train)
+
+#Predict the response for test dataset
+y_pred = knn.predict(X_test)
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("Precision:",metrics.precision_score(y_test, y_pred))
+print("Recall:",metrics.recall_score(y_test, y_pred))
+
+
+# In[ ]:
+
+
+
+
