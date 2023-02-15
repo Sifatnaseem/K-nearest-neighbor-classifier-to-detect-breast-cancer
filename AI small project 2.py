7f80daddb00500408112dcb5f39b05cbf52263d9
SIFAT NASEEM <121250143+Sifatnaseem@users.noreply.github.com>
2023-02-15 11:57:12 -0500

    Update AI small project 2.py

--- AI small project 2.py
+++ AI small project 2.py
@@ -38,7 +38,7 @@ knn.fit(X_train, y_train)
 y_pred = knn.predict(X_test)
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 print("Precision:",metrics.precision_score(y_test, y_pred))
-print("specificity:",metrics.recall_score(y_test, y_pred))
+print("Recall:",metrics.recall_score(y_test, y_pred))
 
 
 
