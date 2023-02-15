705f4d45acca857945e4eac45d057bc6543cbafd
SIFAT NASEEM <121250143+Sifatnaseem@users.noreply.github.com>
2023-02-15 16:00:26 -0500

    Update AI small project 2.py

--- AI small project 2.py
+++ AI small project 2.py
@@ -1,5 +1,4 @@
 from sklearn import datasets
-
 #Load dataset
 cancer = datasets.load_breast_cancer()
 # print the names of the 13 features
@@ -7,17 +6,11 @@ print("Features: ", cancer.feature_names)
 
 # print the label type of cancer('malignant' 'benign')
 print("Labels: ", cancer.target_names)
-
-
-# In[47]:
-
-
 # Import train_test_split function
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn import metrics
-
 # Split dataset into training set and test set
 X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, test_size=0.3,random_state=109) # 70% training and 30% test
 knn=KNeighborsClassifier()
@@ -26,14 +19,8 @@ param_grid = dict(n_neighbors=k_range)
 grid_search=grid.fit(X_train,y_train)
 grid = GridSearchCV (knn, param_grid,cv=5,scoring='accuracy',return_train_score=False,verbose=1)
 print(grid_search.best_params_)
-
-
-# In[45]:
-
-
 knn=  KNeighborsClassifier(n_neighbors=8)
 knn.fit(X_train, y_train)
-
 #Predict the response for test dataset
 y_pred = knn.predict(X_test)
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
